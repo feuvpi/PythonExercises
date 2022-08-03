c213266057556cb3a3b012d1cba68b6ca5e87452
feuvpi <fredvpg@gmail.com>
2022-08-02 23:01:54 -0300

    ex075 tuples

---
+++ ex075.py
@@ -0,0 +1,5 @@
+# Develop a programa that takes four input values and store it into a tuple.
+# Show if and how many times the value "9" shows up
+# Shows in what position is value "3", if any
+# Shows the even numbers
+
