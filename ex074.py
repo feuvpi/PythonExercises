35a3bfe3cc88b673c5d135b62c534f44eda8590b
feuvpi <fredvpg@gmail.com>
2022-08-02 22:06:36 -0300

    initial commit, exercises 72 and 74.

---
+++ ex074.py
@@ -0,0 +1,17 @@
+from numpy import random
+
+# Working with tuplas
+# Create a program that generates five random numbers and place them in a tupla.
+# Show the created numbers and smallest and highest values
+
+# Generate 5 Random numbers into a tupla
+set = (f'{random.randint(100)}', f'{random.randint(100)}', f'{random.randint(100)}', f'{random.randint(100)}', f'{random.randint(100)}',)
+
+# display created numbers
+print(set)
+
+# display minimum value
+print(min(set))
+
+# display maximum value
+print(max(set))
