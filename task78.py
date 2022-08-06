f83038599200a113756bdafdc7bbe7cfcccb4dbc
feuvpi <fredvpg@gmail.com>
2022-08-06 20:51:57 -0300

    task 78 manipulating lists.

---
+++ task78.py
@@ -0,0 +1,5 @@
+# Write a program that reads 5 numeric values ​​and stores them in a list. At the end, show which was
+# the highest and lowest value entered and their respective positions in the list.
+
+list = [int(input('Enter an int: ')), int(input('Enter an int: ')), int(input('Enter an int: ')),
+        int(input('Enter an int: ')), int(input('Enter an int: ')),]
