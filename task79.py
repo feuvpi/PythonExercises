af9313e1453ca183f53e53089c01e653707c10b0
feuvpi <fredvpg@gmail.com>
2022-08-07 23:51:29 -0300

    task79 manipulating lists, using append and sort.

---
+++ task79.py
@@ -0,0 +1,18 @@
+# Create a program where the user can enter several numeric values and register them in a list.
+# If the number already exists in there, it will not be added.
+# At the end, all unique values entered will be displayed, in ascending order
+
+list = []
+for item in range(0, 5):
+    i = 1
+    while i > 0:
+        new_item = int(input('Please enter a number: '))
+        if new_item in list:
+            "Can not repeat values. Digit again."
+        else:
+            list.append(new_item)
+            break
+print('-'*15)
+print('The entered values are:')
+print(list)
+print(list.sort())
